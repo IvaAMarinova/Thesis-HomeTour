40712592416d69577443543e14806d52250d4496
Iva Marinova <ivamarinova06@gmail.com>
2024-12-06 18:31:51 +0200

    Fixed cors issue

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,10 @@ import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true
+  });
   app.use(cookieParser(process.env.COOKIE_SECRET));
   await app.listen(3000);
 }
