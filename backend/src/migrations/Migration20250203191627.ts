389804b2c6a4a56e771129798a754a3dc3d17310
Iva Marinova <ivamarinova06@gmail.com>
2025-02-03 21:22:00 +0200

    Migrated and fixed bug in property and userproperty relation.

---
+++ backend/src/migrations/Migration20250203191627.ts
@@ -0,0 +1,35 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20250203191627 extends Migration {
+
+  async up(): Promise<void> {
+    this.addSql(`
+      ALTER TABLE "user_property"
+      DROP CONSTRAINT IF EXISTS "user_property_property_id_foreign";
+    `);
+
+    this.addSql(`
+      ALTER TABLE "user_property"
+      ADD CONSTRAINT "user_property_property_id_foreign"
+      FOREIGN KEY ("property_id")
+      REFERENCES "property_entity" ("id")
+      ON DELETE CASCADE;
+    `);
+  }
+
+  async down(): Promise<void> {
+    this.addSql(`
+      ALTER TABLE "user_property"
+      DROP CONSTRAINT IF EXISTS "user_property_property_id_foreign";
+    `);
+
+    this.addSql(`
+      ALTER TABLE "user_property"
+      ADD CONSTRAINT "user_property_property_id_foreign"
+      FOREIGN KEY ("property_id")
+      REFERENCES "property_entity" ("id")
+      ON DELETE NO ACTION;
+    `);
+  }
+
+}
